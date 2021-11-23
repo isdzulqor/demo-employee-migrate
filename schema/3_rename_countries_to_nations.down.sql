f20568054116b978b839ed3f161750fd7ba7f1d2
isdzulqor <midzulqornain@gmail.com>
2021-11-23 14:12:58 +0700

    db: rename countries to nations

---
+++ schema/3_rename_countries_to_nations.down.sql
@@ -0,0 +1,10 @@
+-- THIS IS DANGEROUS, if we have millions of data
+-- it will lock the transaction
+-- RENAME TABLE <old_table_name> TO <new_table_name>
+-- RENAME TABLE countries TO nations
+
+CREATE TABLE countries
+     SELECT * FROM nations;
+
+DROP TABLE nations;
+
---
+++ schema/3_rename_countries_to_nations.up.sql
@@ -0,0 +1,10 @@
+-- THIS IS DANGEROUS, if we have millions of data
+-- it will lock the transaction
+-- RENAME TABLE <old_table_name> TO <new_table_name>
+-- RENAME TABLE countries TO nations
+
+CREATE TABLE nations
+     SELECT * FROM countries;
+
+DROP TABLE countries;
+
